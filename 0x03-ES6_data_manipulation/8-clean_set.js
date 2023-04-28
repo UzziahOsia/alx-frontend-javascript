53b570550a2c9297ed54d171b7c0a1c67529ff8f
Uzziah Osia <85574539+UzziahOsia@users.noreply.github.com>
2023-04-28 15:42:51 +0100

    cleared whitespaces

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,6 +4,5 @@ export default function cleanSet(set, startString) {
   set.forEach((i) => {
     if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
   });
-  
-  return result.slice(0, result.length - 1);
+   return result.slice(0, result.length - 1);
 }
